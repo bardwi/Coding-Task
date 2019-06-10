34eb2a6f949588de56ac7792c3d561fc58eb41da
Bidansghree brahma <bardwi@bidangshrees-MacBook-Air.local>
2019-06-11 02:24:04 +0530

    Git commit

--- apiServer.js
+++ apiServer.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // APIs
 
 
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = "mongodb+srv://bardwi:@Alicante123@cluster0-zbdsg.mongodb.net/test?retryWrites=true&w=majority";
@@ -48,8 +48,8 @@ app.use(session({
     saveUninitialized:true,
     resave:false,
     //cookie:{maxAge: 1000 * 60 * 60 * 24 * 2},
-    cookie: {secure: false},
-    store: new MongoStore({mongooseConnection: db, ttl: 2 * 24 * 60 * 60})
+    //cookie: {secure: false},
+    //store: new MongoStore({mongooseConnection: db, ttl: 2 * 24 * 60 * 60})
 }))
 
 
